fae52faa1aef49568281a3eb7116c4026cf38f86
DionAhza <dionahza15@gmail.com>
2025-07-19 15:51:24 +0700

    perbaruan 19 juli

--- app/layout.tsx
+++ app/layout.tsx
@@ -13,10 +13,39 @@ const geistMono = localFont({
   weight: "100 900",
 });
 
-export const metadata: Metadata = {
-  title: "DionAhza",
-  description: "Portofolio Website Dion Ahza Rabbani",
+// app/layout.tsx atau app/page.tsx
+export const metadata = {
+  title: "DionAhza | Portofolio",
+  description: "Website portofolio Dion Ahza Rabbani — berisi tentang saya, projek, sertifikat, dan kontak.",
+  keywords: ["Dion Ahza", "Portofolio", "Web Developer", "React", "Laravel", "Next.js"],
+  authors: [{ name: "Dion Ahza Rabbani", url: "https://dionahzarabbani.vercel.app" }],
+  creator: "Dion Ahza Rabbani",
+  metadataBase: new URL("https://dionahzarabbani.vercel.app"),
+  openGraph: {
+    title: "DionAhza | Portofolio",
+    description: "Dion Ahza Rabbani Portfolio Website",
+    url: "https://dionahzarabbani.vercel.app",
+    siteName: "DionAhza Portfolio",
+    images: [
+      {
+        url: "/assets/GambarDion.png", // pastikan gambar ini ada di folder public
+        width: 1200,
+        height: 630,
+        alt: "Preview Portofolio Dion Ahza",
+      },
+    ],
+    type: "website",
+  },
+  robots: {
+    index: true,
+    follow: true,
+    googleBot: {
+      index: true,
+      follow: true,
+    },
+  },
 };
+
 
 export default function RootLayout({
   children,
